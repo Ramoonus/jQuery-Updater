79ae6a71f978f3f9b0bc9f76d8a57a7780875866
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-05-03 12:20:38 +0000

    v3.4.1 in trunk
    git-svn-id: https://plugins.svn.wordpress.org/jquery-updater/trunk@2079873 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-updater.php
+++ jquery-updater.php
@@ -4,7 +4,7 @@
  * Plugin Name: jQuery Updater
  * Plugin URI: http://www.ramoonus.nl/wordpress/jquery-updater/
  * Description: This plugin updates jQuery to the latest  stable version.
- * Version: 3.4.0
+ * Version: 3.4.1
  * Author: Ramoonus
  * Author URI: http://www.ramoonus.nl/
  * License: GPL3
@@ -25,7 +25,7 @@ function rw_jquery_updater()
     // Deregister core jQuery
     wp_deregister_script('jquery');
     // Register
-    wp_enqueue_script('jquery', plugins_url('/js/jquery-3.4.0.min.js', __FILE__), false, '3.4.0');
+    wp_enqueue_script('jquery', plugins_url('/js/jquery-3.4.1.min.js', __FILE__), false, '3.4.1');
 
     // jQuery Migrate
     // Deregister core jQuery Migrate
